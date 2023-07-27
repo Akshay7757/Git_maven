a89be3bf6528eb02641f5cc0f93ead08702681b2
Your Name <you@example.com>
2023-07-27 12:31:47 +0530

    new changes

--- Git_Maven/src/test/java/Simple_progam/First.java
+++ Git_Maven/src/test/java/Simple_progam/First.java
@@ -1,11 +1,13 @@
 package Simple_progam;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Welcome");
-		System.out.println("hiii");
+		WebDriver d = new ChromeDriver();
+		d.get("http://www.saucedemo.com");
 
 	}
 
